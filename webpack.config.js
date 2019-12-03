6180bb9faebc8423fc73e77cab997589f33391cf
Juan Barboza <juabarboza49@gmail.com>
2019-12-03 00:18:21 -0300

    Se agrega babel y webpack con custom plugin

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.css$/, 
+                loader: [
+                  MiniCSSExtractPlugin.loader,
+                  "css-loader"
+                ]
+              },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            { 
+                test: /\.scss$/, 
+                loader: [
+                  MiniCSSExtractPlugin.loader,
+                  "css-loader",
+                  'sass-loader'
+                ]
+              }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: "estilos.css",
+        }), 
+    ]
+};
