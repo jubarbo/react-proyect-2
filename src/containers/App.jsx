f7994c2defad4c6889f5adb40e54e60f3d123202
Juan Barboza <juabarboza49@gmail.com>
2019-12-04 00:27:28 -0300

    hook useState and useEffect, incomplet

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -8,43 +8,55 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
 
-        <Categories title="Lista 1">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    const [videos , setVideos] = useState([]);
 
-        <Categories title="Lista 4">
-            <Carousel>
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    }, []);
+    console.log(videos.trends.length);
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+        
+        
+            <Categories title="Lista 1">
+                <Carousel>
                 <CarouselItem />
                 <CarouselItem />
-            </Carousel>
-        </Categories>
+                <CarouselItem />
 
-        <Categories title="Lista veinticinco">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Footer />
-    </div>
-)
+                 
+                </Carousel>
+            </Categories>
+            }
+    
+            <Categories title="Lista 4">
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+    
+            <Categories title="Lista veinticinco">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+    
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
